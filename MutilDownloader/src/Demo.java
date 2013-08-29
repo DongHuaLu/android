0ff71773ee36c37cb6a2158fe8a001f7cd067dd9
dlu <dlu@suzhoukada.com>
2013-08-29 18:12:34 +0800

    1

--- MutilDownloader/src/Demo.java
+++ MutilDownloader/src/Demo.java
@@ -1,4 +1,5 @@
 import java.io.File;
+import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.InputStream;
 import java.io.RandomAccessFile;
@@ -10,7 +11,8 @@ public class Demo {
 	/**
 	 * @param args
 	 */
-	public static int threadCount = 3;
+	public static int threadCount = 5;
+	public static int runningThread = 0;
 
 	public static void main(String[] args) {
 		// 1.连接服务器,获取文件长度,在本地创建一个大小一样的空文件
@@ -71,13 +73,27 @@ public class Demo {
 
 		@Override
 		public void run() {
+			runningThread++;
 			try {
+				File f = new File(threadId + ".txt");
+				if (f.exists() && f.length() > 0) {
+					FileInputStream fis = new FileInputStream(f);
+					byte[] info = new byte[1024];
+					int leng = fis.read(info);
+					int downloadedLength = Integer.parseInt(new String(info, 0,
+							leng));
+					startIndex = downloadedLength;
+					fis.close();
+				}
+
 				URL url = new URL(path);
 				HttpURLConnection conn = (HttpURLConnection) url
 						.openConnection();
 				// 请求头,得到部分的文件
 				conn.setRequestProperty("Range", "bytes=" + startIndex + "-"
 						+ endIndex);
+				System.out.println("线程" + threadId + "真实下载长度" + startIndex
+						+ "---->" + endIndex);
 				conn.setReadTimeout(5000);
 				conn.setRequestMethod("GET");
 				int code = conn.getResponseCode();
@@ -90,21 +106,30 @@ public class Demo {
 
 				int len = 0;
 				byte[] buff = new byte[1024];
-				//记录当前线程下载的长度
-				int total=0;
-				File file=new File(threadId+".txt");
+				// 记录当前线程下载的长度
+				int total = 0;
+				File file = new File(threadId + ".txt");
 				while ((len = in.read(buff)) != -1) {
-					FileOutputStream fos=new FileOutputStream(file);
+					RandomAccessFile info = new RandomAccessFile(file, "rwd");
 					raf.write(buff, 0, len);
-					total+=len;
-					fos.write(String.valueOf(total).getBytes());
-					fos.close();
+					total += len;
+					info.write(String.valueOf(total + startIndex).getBytes());
+					info.close();
 				}
 				in.close();
 				raf.close();
 				System.out.println("线程" + threadId + "下载完成");
 			} catch (Exception e) {
 				e.printStackTrace();
+			} finally {
+				runningThread--;
+				if (runningThread == 0) {
+					for (int i = 1; i <= threadCount; i++) {
+						File deleteFile = new File(i + ".txt");
+						deleteFile.delete();
+					}
+					System.out.println("文件下载完毕");
+				}
 			}
 		}
 
